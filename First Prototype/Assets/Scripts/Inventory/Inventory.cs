f8fc4a540829f48ea9536e3fc68406129b1eaf02
CiaranHammond <ciaranhammond6895@gmail.com>
2019-10-25 23:01:00 +0100

    First Prototype
    ThirsPersonCamera, ThirdPersonController, Interactable, Pickups, Inventory all set up

---
+++ First Prototype/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    #region Singleton
+    public static Inventory instance;
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("More than one instance of Inventory found");
+        }
+        instance = this;
+    }
+    #endregion
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+    public int space = 6;
+    public List<Item> items = new List<Item>();
+
+    public bool Add(Item item)
+    {
+        if(items.Count >= space)
+        {
+            Debug.Log("Not enough room");
+            return false;//this bool is to not destroy the gameObject if out of room
+        }
+        items.Add(item);
+        
+        if(onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+
+        return true;//this bool is to destroy the gameObject - i.e added to inv
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
---
+++ First Prototype/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+    public Interactable focus;
+    public float Speed;
+
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+        if(Input.GetMouseButtonDown(0))
+        {
+            RemoveFocus();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //Debug.Log(interactable);
+            if(Physics.Raycast(ray, out hit))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    //Debug.Log("Right Click Confirm");
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver).normalized * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if(newFocus != focus)
+        {
+            if(focus != null)
+            {
+                focus.OnDefocused();
+            }
+            focus = newFocus;
+        }
+        
+        newFocus.OnFocused(transform);
+    }
+
+    void RemoveFocus()
+    {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+        focus = null;
+    }
+}
