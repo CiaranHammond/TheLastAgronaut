5a857db02b56bf6b1438ab02d7d6228ff090f0f5
KerrieC98 <38563192+KerrieC98@users.noreply.github.com>
2020-02-10 14:18:48 +0000

    Added battle system
    Added turn based combat and scene transitions

---
+++ First Prototype/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+    public BattleState state;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public BattleHUDScript playerHUD;
+    public BattleHUDScript enemyHUD;
+
+    public GameObject levelChanger;
+    public GameObject objectManager;
+
+    public Text dialogueText;
+
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
+
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "A wild " + enemyUnit.unitName + " approaches!";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "What will PENELOPE do?";
+    }
+
+    public void onAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void onHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerHeal());
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(5);
+        playerHUD.SetHP(playerUnit.currentHP);
+        dialogueText.text = "Penelope feels renewed strength!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+
+    IEnumerator PlayerAttack()
+    {
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.text = "Penelope stabs MUTATED PLANT with a pitchfork!";
+
+        yield return new WaitForSeconds(2f);
+
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }else
+        {
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " spits an unknown substance at Penelope!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(2f);
+
+        if(isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if(state == BattleState.WON)
+        {
+            dialogueText.text = "You won the battle!";
+        }
+        else if(state == BattleState.LOST)
+        {
+            dialogueText.text = "You were defeated!";
+        }
+
+        LevelChanger lc = levelChanger.GetComponent<LevelChanger>();
+        
+        lc.FadeToLevel(0);
+    }
+}
